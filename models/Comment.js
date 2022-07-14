6c5729826575de781d08b03894bf7369b43126ac
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-14 16:30:33 -0400

    updated comment model to include reply schema, added getters for date formatting in comment model, created virtual for counting # of replies, updated pizza virtual to count replies and comments using "reduce()"

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,31 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with paren comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const CommentSchema = new Schema(
   {
@@ -11,9 +38,22 @@ const CommentSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema],
   },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
+
+CommentSchema.virtual("replyCount").get(function () {
+  return this.replies.length;
+});
 
 const Comment = model("Comment", CommentSchema);
 
