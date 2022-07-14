cd21bc98337e7a3654912997ee9fa6a7b82237c5
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-14 16:14:55 -0400

    generated single pizza page, added functionality to add new comments on a pizza

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,20 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+);
 
-const Comment = model('Comment', CommentSchema);
+const Comment = model("Comment", CommentSchema);
 
 module.exports = Comment;
